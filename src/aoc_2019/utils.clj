b6a7053bd8faae71d8ceac472573eca08bb57574
Marc Jeanson <marc@redlinesoftware.com>
2019-12-10 00:13:35 +0100

    Helper method to read data from a file used by intcode

--- src/aoc_2019/utils.clj
+++ src/aoc_2019/utils.clj
@@ -1,8 +1,15 @@
 (ns aoc-2019.utils
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn read-file
   [file-name]
   (->> (io/reader file-name)
        line-seq
        (map #(read-string %))))
+
+(defn read-file-to-vector
+  [filename]
+  (mapv #(read-string %) (-> (io/resource filename)
+                             (slurp)
+                             (str/split #","))))
