25b6b98b4e8d12462ba10d9be40bb66b46111478
Marc Jeanson <marc@redlinesoftware.com>
2019-12-10 00:12:53 +0100

    Added a dependency for doing permutations

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject aoc-2019 "0.1.0-SNAPSHOT"
   :description "Advent of Code 2019"
   :url "https://adventofcode.com"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :main ^:skip-aot aoc-2019.core
   :target-path "target/%s"
   :resource-paths ["resources"]
